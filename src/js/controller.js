b1d4a421fe34686e515aa5be7552573b3ca9d870
valeria <valeria.penaloza5@gmail.com>
2018-08-18 11:19:13 -0300

    corrección de indentación

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,28 +2,28 @@
 window.controller = {};
 
 window.onload = () => {
-	setTimeout (() => {
-		const containerPreloader = document.getElementById('containerPreloader');
-		containerPreloader.style.visibility = 'hidden';
-		containerPreloader.style.opacity = '0';
-	}, 2000);
-	window.view.index();
+  setTimeout(() => {
+    const containerPreloader = document.getElementById('containerPreloader');
+    containerPreloader.style.visibility = 'hidden';
+    containerPreloader.style.opacity = '0';
+  }, 2000);
+  window.view.index();
 };
 
 window.controller.answersRest = () => {
-	const answerRest = window.model.filterRest();
-	window.view.onSearchBoxChange(answerRest);
-}
+  const answerRest = window.model.filterRest();
+  window.view.onSearchBoxChange(answerRest);
+};
 
 window.controller.answersCategory = () => {
-	const answerCategory = window.model.filterCategory();
-	window.view.onSearchCategory(answerCategory);
-}
+  const answerCategory = window.model.filterCategory();
+  window.view.onSearchCategory(answerCategory);
+};
 
 window.controller.controllerFilterRest = (localRest, search) => {
-	return window.model.filterRest(localRest, search);
-}
+  return window.model.filterRest(localRest, search);
+};
 
 window.controller.controllerFilterCategory = (localRest, search) => {
-	return window.model.filterCategory(localRest, search);
-}
+  return window.model.filterCategory(localRest, search);
+};
--- src/js/model.js
+++ src/js/model.js
@@ -6,52 +6,51 @@ let restaurant = null;
 let localRest = null;
 let search = null;
 
-//llamando json
+// llamando json
 fetch('src/js/restaurantes.json')
-.then(response => response.json())
-.then(restaurantesJSON => {
-	restaurant = restaurantesJSON;
-	localRest = window.model.restName(restaurant);
-	// console.log(localRest);
-})
-.catch(error => {
-	console.error("No pudimos obtener respuesta");
-	console.error("ERROR > " + error.stack);
-});
+  .then(response => response.json())
+  .then(restaurantesJSON => {
+    restaurant = restaurantesJSON;
+    localRest = window.model.restName(restaurant);
+    // console.log(localRest);
+  })
+  .catch(error => {
+    console.error('No pudimos obtener respuesta');
+    console.error('ERROR > ' + error.stack);
+  });
 
 //  recorriendo json
 window.model.restName = (restaurant) => {
-	for (let i in restaurant){
-		let nameRest = restaurant[i].name;
-	}
-	return restaurant;
-}
+  for (let i in restaurant) {
+    let nameRest = restaurant[i].name;
+  }
+  return restaurant;
+};
 
 // filtro de locales por nombre
 window.model.filterRest = (localRest, search) => {
-	if(search){
-		if(localRest){
-			search = search.toLowerCase();
-			return  localRest.filter(rest => rest &&
+  if (search) {
+    if (localRest) {
+      search = search.toLowerCase();
+      return localRest.filter(rest => rest &&
 				rest.name &&
-				rest.name.toLowerCase().indexOf(search)>= 0);
-		}
-	}
-	return localRest;
+				rest.name.toLowerCase().indexOf(search) >= 0);
+    }
+  }
+  return localRest;
 };
 
 // filtrando locales por categoria
 window.model.filterCategory = (localRest, search) => {
-	if(search){
-		if(localRest){
-			search = search.toLowerCase();
-			return  localRest.filter(rest => rest &&
+  if (search) {
+    if (localRest) {
+      search = search.toLowerCase();
+      return localRest.filter(rest => rest &&
 				rest.category &&
-				rest.category.toLowerCase().indexOf(search)>= 0);
-		}
-	}
-	return localRest;
+				rest.category.toLowerCase().indexOf(search) >= 0);
+    }
+  }
+  return localRest;
 };
-
 
 
